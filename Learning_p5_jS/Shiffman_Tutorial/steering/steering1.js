5b1c74af504dd238ec8a53825b527535a29f0d0c
gamontec <gamontec@uc.cl>
2017-07-06 19:23:48 -0400

    taller1.py + steering

---
+++ Learning_p5_jS/Shiffman_Tutorial/steering/steering1.js
@@ -0,0 +1,19 @@
+//Guillermo Montecinos
+//06/07/2017
+//aprendiendo steering desde el libro The Nature of Code
+
+function setup(){
+  createCanvas(800,500);
+}
+
+function draw(){
+
+}
+
+//Clase vehículo
+function Vehicle(){
+  this.location = createVector();
+  this.velocity = createVector();
+  this.acceleration = createVector();
+
+}
--- taller_escritura_creativa/sesion_1.py
+++ taller_escritura_creativa/sesion_1.py
@@ -1,3 +1,6 @@
 import random
-poema = " bytes de amor y un bit deseperado"
-print str(random.random()) + poema
+
+x = [5, 10, 15, 20, 25,30] #esto es una lista, no un arreglo
+print x[2]
+print len(x)
+print random.choice(x)
