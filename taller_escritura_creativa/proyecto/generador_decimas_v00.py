0959202d6382a26bcf75529dd2ee52f2dba8644c
gamontec <gamontec@uc.cl>
2017-07-19 21:02:40 -0400

    proyecto

--- taller_escritura_creativa/proyecto/generador_decimas_v00.py
+++ taller_escritura_creativa/proyecto/generador_decimas_v00.py
@@ -4,6 +4,7 @@ proyecto final
 taller de escritura creativa
 codedEscuela
 por guillermo montecinos
+19/07/17
 v 0.0
 '''
 '''
@@ -11,5 +12,5 @@ el proyecto consiste escribir un software que
 genere versos en decima
 metodo:
 definir estructura gramatical sobre  la que se escriben decimas
-definir reglas de verso en decima 
+definir reglas de verso en decima
 '''
