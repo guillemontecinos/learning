12fc18ec3e47a00a785b190dc1447f8b804c1fc8
gamontec <gamontec@uc.cl>
2017-07-19 21:01:55 -0400

    pryect definition

--- taller_escritura_creativa/proyecto/generador_decimas_v00.py
+++ taller_escritura_creativa/proyecto/generador_decimas_v00.py
@@ -1,8 +1,15 @@
 '''
-generador de versos en décima
+generador de versos en decima
 proyecto final
 taller de escritura creativa
 codedEscuela
 por guillermo montecinos
 v 0.0
+'''
+'''
+el proyecto consiste escribir un software que
+genere versos en decima
+metodo:
+definir estructura gramatical sobre  la que se escriben decimas
+definir reglas de verso en decima 
 '''
