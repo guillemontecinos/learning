8e33d1b12f4b248b0ddaf57cbb94942ba8953b76
gamontec <gamontec@uc.cl>
2017-07-19 21:06:38 -0400

    proyecto

--- taller_escritura_creativa/proyecto/generador_decimas_v00.py
+++ taller_escritura_creativa/proyecto/generador_decimas_v00.py
@@ -4,6 +4,7 @@ proyecto final
 taller de escritura creativa
 codedEscuela
 por guillermo montecinos
+ref: http://www.scielo.cl/scielo.php?script=sci_arttext&pid=S0716-27902000019400007
 19/07/17
 v 0.0
 '''
